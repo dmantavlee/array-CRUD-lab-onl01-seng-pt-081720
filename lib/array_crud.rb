57a27821e63055546053cd0d8fc248b05cc5adcd
Jason Beckstead <jason_beckstead@yahoo.com>
2020-08-23 21:27:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-["mom", "dylan", "tavia", "kinlee"]
+ ["mom", "dylan", "tavia", "kinlee"]
 end
 
 def add_element_to_end_of_array(array, element)
